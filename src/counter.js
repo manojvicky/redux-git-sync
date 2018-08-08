d048f02ccb6ff4642f4d47bdc3016404e02ee9ca
manojvicky <manoj.save.earth@gmail.com>
2018-08-08 18:30:47 +0530

    this to other js file

--- src/counter.js
+++ src/counter.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import otherFunction from './otherFunction';
 
 class Counter extends React.Component {
     constructor(){
         super();
+        this.state={
+            kiki:""
+        }
         this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-        
+        this.hello = this.decrement.bind(this);
+        this.newFunction = otherFunction.bind(this);
     }
     increment(){
         this.props.actions.increment(this.props.data+1);
-};
-    decrement(){;
-this.props.actions.increment(this.props.data-1);  
-};
+        this.newFunction("hello", "manoj ji");
+    }
+    decrement(str){
+        console.log("decrement", str); 
+        return str;
+    }
+
+    // handleClick = () => {
+    //     console.log("arrow function");
+    // }
+
+    // arrowFunction = () => {
+    //     console.log("arrow function");
+    // };
    render() {
+       console.log("this", this);
+       
       return (
          <div>
              <div>
-            <button onClick={ this.increment } >+</button>
-            <button onClick={ this.decrement }>-</button>
+            <button onClick={ this.increment } >Increase</button>
+            <button onClick={ this.hello }>Decrease</button>
             </div>
             <p>{this.props.data}</p>
          </div>
---
+++ src/otherFunction.js
@@ -0,0 +1,7 @@
+export default function(salutation, name){
+    
+    console.log("this of function", salutation, name, this);
+    this.setState({
+        kiki: "kiki"
+    })
+}
